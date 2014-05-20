e7c25f80e9c9da8e861b5bb5a4204da83481a7d4
venkat <vpodugu@gmail.com>
2014-05-20 11:16:22 -0400

    All routes defined in this new file.

--- linkneighbors/routes/allroutes.js
+++ linkneighbors/routes/allroutes.js
@@ -1,3 +1,32 @@
 /**
- * New node file
+ * Define all the routes being used in the project here
  */
+
+export.index = function(request, response) {
+	response.send('index', title: 'Index');
+};
+	
+export.login = function(request, response) {
+	response.send('signin', title: 'Login');
+};
+
+export.signup = function(request, response) {
+	response.send('signup', title: 'Create Account');
+};
+
+export.group = function(request, response) {
+	response.send('signin', title: 'Login');
+};
+
+export.about = function(request, response) {
+	response.send('about', title: 'About');
+};
+
+export.blog = function(request, response) {
+	response.send('blog', title: 'Blog');
+};
+
+export.contact = function(request, response) {
+	response.send('contact', title: 'Contact');
+};
+

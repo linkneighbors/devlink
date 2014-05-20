908e1132369a0510c45a67d780e0cacd0cf050e8
venkat <vpodugu@gmail.com>
2014-05-20 16:29:17 -0400

    Multiple Changes.
    - All invalid routes handled
    - Sample Login Verification added.
    - Unauthorized access page.

--- linkneighbors/app.js
+++ linkneighbors/app.js
@@ -12,6 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -27,18 +28,112 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//This is a middleware that we will use on routes where
+//we _require_ that a user is logged in, such as the /secret url
+function requireUser(req, res, next) {
+	if (!req.user) {
+		res.redirect('/not_allowed');
+	} else {
+		next();
+	}
+}
+
+
 app.get('/users', user.list);
 
 app.get('/', routes.index);
 app.get('/index', routes.index);
 app.get('/signin', routes.signin);
 app.get('/about', routes.about);
-app.get('/group', routes.group);
 app.get('/signup', routes.signup);
 app.get('/contact', routes.contact);
 app.get('/blog', routes.blog);
+app.get('/unauthorized', routes.unauthorized);
 
+
+function requireUser(request, response, next) {
+	
+	if (!request.user) {
+		response.redirect('/unauthorized');
+	} else {
+		next();
+	}
+
+}
+
+// This page cannot be access without successful login
+app.get('/group', requireUser, routes.group);
+
+
+// It's dummy for now
+// Use Mongo DB to validate the username & password
+// return callback(null,null) in case of failure
+// return callback(null,user) in case of success
+
+function authenticateUser(username, password, callback) {
+	
+	console.log("User Validation Successful");
+	// for now, send a default success response
+	
+	if( (username == "vpodugu@gmail.com") && (password == "venkat") ) {
+	
+		user.username = "vpodugu@gmail.com";
+		return callback(null,user)
+	
+	} else {
+		error = "Invalid Credentials!!!"
+	}
+	
+	return callback(error, null);
+	
+}
+
+// This is the POST request from signin Page
+// Receives username & password
+// Validate the user and redirect to group Page
+
+app.post('/login', function(request, response) {
+	
+	console.log("POST request received");
+	
+	var _user = request.body.email;
+	var _password = request.body.password;
+	
+	console.log("User Name " + _user);
+	console.log("Password " + _password);
+	
+	//Authenticate the user
+	authenticateUser(_user, _password, function(err,user) {
+		
+		if(user) {
+		
+			// the validation is successful
+			// redirect to authorized users page.
+			response.render("group");
+		
+		} else {
+		
+			// user validation failed.
+			console.log("Authentication failed : " + err);
+			response.render("signin", { error : err } );
+		}
+		
+	});
+	
+	response.render("signin");
+
+});
+
+
+// Create HTTP Server and listen on port
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// This will redirect all the invalid routes to main page
+
+app.use(function(req, res) {
+    res.redirect('/')
+});
+
--- linkneighbors/routes/allroutes.js
+++ linkneighbors/routes/allroutes.js
@@ -3,30 +3,45 @@
  */
 
 exports.index = function(request, response) {
+	// This loads views/index.ejs file
 	response.render('index');
 };
 	
 exports.signin = function(request, response) {
+	//This loads views/signin.ejs file
 	response.render('signin');
 };
 
 exports.signup = function(request, response) {
+	//This loads views/signup.ejs file
 	response.render('signup');
 };
 
 exports.group = function(request, response) {
+	//This loads views/group.ejs file
 	response.render('group');
 };
 
 exports.about = function(request, response) {
+	//This loads views/about.ejs file
 	response.render('about');
 };
 
 exports.blog = function(request, response) {
+	//This loads views/blog.ejs file
 	response.render('blog');
 };
 
 exports.contact = function(request, response) {
+	//This loads views/contact.ejs file
 	response.render('contact');
 };
 
+exports.error = function(request, response) {
+	//This is error landing page
+	response.send("Oh no!", 404);
+};
+
+exports.unauthorized = function(request, response) {
+	response.render('unauthorized');
+};
